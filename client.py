c5a52348b75d96972296f57ae34af66eea7810eb
Ajinaline <otokpa@my.yorku.ca>
2025-06-22 21:42:26 -0400

    Added client script
    This is for the client to assign their nickname, receive messages and type theirs

---
+++ client.py
@@ -0,0 +1,24 @@
+import socket
+import threading
+
+nickname = input("Choose a nickname: ")
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 55668))
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('ascii')
+            if message == 'NICK':
+                client.send(nickname.encode('ascii'))
+            else:
+                print(message)
+        except:
+            print("An error occurred!")
+            client.close()
+            break
+        
+def write():
+    while True:
+        message = f"{nickname}: {input("")}"
